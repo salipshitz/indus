97fd7002005c6650f71c27db34fc1562d46a1d40
geremachek <mrender2005@gmail.com>
2019-01-06 09:56:08 -0800

    Add files via upload

---
+++ indus.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+from curses import *
+from sys import *
+
+# Other
+
+s = initscr()
+start_color()
+use_default_colors()
+noecho()
+s.keypad(1)
+
+fg, bg = 0, -1
+
+for i in range(0, 256):
+    init_pair(i + 1, i, bg)
+
+# Varibles
+
+# Buffers
+fileBuff = [] # Entire file buffer
+lineBuff = [] # Entire line buffer
+
+# Other
+maxY, maxX = s.getmaxyx() # Screen size
+currY, currX = 0, 0 # Current coordinates
+currLine = 0 # Current line
+currLineIndex = 0 # Curent line index
+
+# Functions
+
+def updateArr(index, arr, arrApp): # Update arr at index
+    del arr[index]
+    arr.insert(index, arrApp)
+
+def addArr(index, arr, arrApp):
+    arr.insert(index, arrApp)
+
+def updateScr(key):
+    global currY, currX
+    s.move(currY, currX)
+    s.addstr(key)
+    currX += 1
+
+def main():
+main()
